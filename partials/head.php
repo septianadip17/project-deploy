3069c6f06cedd601c3ef93e38aaf74359329b41f
Septian Adi Pratama <septianadip7998@gmail.com>
2025-02-06 09:07:38 +0700

    icon raed

--- partials/head.php
+++ partials/head.php
@@ -8,6 +8,8 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title><?= $title ?? 'Sistem Deployment' ?></title>
   <script src="https://cdn.tailwindcss.com"></script>
+  <link rel="icon" href="../assets/logo.png" type="image/png">
+
 </head>
 
 <body class="bg-gray-100">
